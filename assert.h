63996b5131e1e41ad11d0eba00b6978c31541d54
hailey-royle <hailey.royle@proton.me>
2026-06-15 14:38:58 -0600

    add assert.h to git tracking

--- assert.h
+++ assert.h
@@ -3,8 +3,10 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 
+#ifdef NO_ASSERT
+#define Assert( expr, ... ){( void )( expr ); }
+#else
 #define Assert( expr, ... ){ \
 	if( !( expr )){ \
 		fprintf( stderr, "%s:%d: %s: Assertion \"%s\" failed. ", __FILE__, __LINE__, __func__, #expr ); \
@@ -14,13 +16,18 @@
 		abort(); \
 	} \
 }
+#endif
 
 #define Unreachable() Assert( 0, "Unreachable." );
 
+#ifdef NO_ASSERT
+#define DebugPrintf( ... ){( void )( 0 ); }
+#else
 #define DebugPrintf( ... ){ \
 	fprintf( stderr, "%s:%d: %s ", __FILE__, __LINE__, __func__ ); \
 	fprintf( stderr, __VA_ARGS__ ); \
 	fflush( stderr ); \
-} 
+}
+#endif
 
 #endif
